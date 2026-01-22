44432ace4e4cf4137bb4d2024ab3d8a97dd0325c
Suganya Maheswaran <43247716+Suganya1990@users.noreply.github.com>
2026-01-22 18:53:15 -0500

    feat(filter):add TypeScript logic for fitler componet

--- REST_Countries_API_CRUD_APP/Countries_CRUD_Frontend/app/src/app/filter/filter.component.ts
+++ REST_Countries_API_CRUD_APP/Countries_CRUD_Frontend/app/src/app/filter/filter.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-filter',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './filter.component.html',
   styleUrl: './filter.component.css'
 })
 export class FilterComponent {
+  region = ['Africa', 'America', 'Asia', 'Europe', 'Oceania' ];
+  dropdownOpen = false;
+  selectedRegion = '';
 
+  toggleDropdown(){
+    this.dropdownOpen = !this.dropdownOpen;
+    console.log("ToggleDropDown : ", this.dropdownOpen)
+  }
+
+  onFilterChange(region:string){
+    this.selectedRegion = region;
+    this.dropdownOpen = false; 
+  }
 }
