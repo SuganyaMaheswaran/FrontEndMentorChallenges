08ff707dfeb7a5db90d8e6b9984abd64d418ee94
Suganya Maheswaran <43247716+Suganya1990@users.noreply.github.com>
2026-01-22 19:12:13 -0500

    add component

--- REST_Countries_API_CRUD_APP/Countries_CRUD_Frontend/app/src/app/component/card-container/card-container.component.ts
+++ REST_Countries_API_CRUD_APP/Countries_CRUD_Frontend/app/src/app/component/card-container/card-container.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-
+import { CardComponent } from '../card/card.component';
 @Component({
   selector: 'app-card-container',
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './card-container.component.html',
   styleUrl: './card-container.component.css'
 })
