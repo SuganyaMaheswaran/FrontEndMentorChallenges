08f6d321e6f2f57f8ed029bdf3bb0a98f83d4aa5
Suganya Maheswaran <43247716+Suganya1990@users.noreply.github.com>
2026-01-23 17:00:22 -0500

    feat: added card component

--- REST_Countries_API_CRUD_APP/Countries_CRUD_Frontend/app/src/app/component/card/card.component.ts
+++ REST_Countries_API_CRUD_APP/Countries_CRUD_Frontend/app/src/app/component/card/card.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Country } from '../../models/country.model';
 
 @Component({
   selector: 'app-card',
@@ -7,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrl: './card.component.css'
 })
 export class CardComponent {
+@Input() country?:Country; 
 
 }
