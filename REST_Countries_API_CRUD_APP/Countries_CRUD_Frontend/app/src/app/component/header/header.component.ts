8ded3c937170f65e842ccd59d4c45c4356c04f7e
Suganya Maheswaran <43247716+Suganya1990@users.noreply.github.com>
2026-01-22 19:00:33 -0500

    rearranged file structure

---
+++ REST_Countries_API_CRUD_APP/Countries_CRUD_Frontend/app/src/app/component/header/header.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faMoon } from '@fortawesome/free-solid-svg-icons';
+import { ThemeService } from '../../theme.service';
+@Component({
+  selector: 'app-header',
+  imports: [FontAwesomeModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+ 
+  faMoon = faMoon;
+  constructor(private themeService:ThemeService){}
+  toggleTheme(){
+    // console.log("Togglebutton clicked")
+    this.themeService.toggleTheme();
+  }
+
+}
