b309f4362d86a8c073eb10325df4807404671b84
Suganya Maheswaran <43247716+Suganya1990@users.noreply.github.com>
2026-02-02 19:01:24 -0500

    added routing module

---
+++ Countries_CRUD_Frontend/app/src/app/app-routing.module.ts
@@ -0,0 +1,4 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { CardContainerComponent } from "./component/card-container/card-container.component";
+import { CardDetailComponent } from "./component/card-detail/card-detail.component";
